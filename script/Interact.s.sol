fa3f8e837cd2220aafab597b0d5007dfa9e28ed3
Phạm Văn Bảo Huy <huypham@MacBook-Pro-cua-Pham-2.local>
2025-10-22 09:14:59 +0700

    Welcome to my Capstone

---
+++ script/Interact.s.sol
@@ -0,0 +1,45 @@
+// SPDX-Licence-Indentifier: MIT
+pragma solidity ^0.8.24;
+
+import {Script, console} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+
+contract ClaimAirdrop is Script {
+    address private constant CLAIMING_ADDRESS = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    uint256 private constant AMOUNT_TO_COLLECT = (25 * 1e18); // 25.000000
+
+    bytes32 private constant PROOF_ONE = 0xd1445c931158119b00449ffcac3c947d028c0c359c34a6646d95962b3b55c6ad;
+    bytes32 private constant PROOF_TWO = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] private proof = [PROOF_ONE, PROOF_TWO];
+
+    bytes private SIGNATURE =
+        hex"f7db10e1c0cd505d6bd2ba83aa2f793d843c1f5f873e1a2b55d79da0171de4344bcb606cb2c3b5620257ce72189f39a5ce1712cb7118ce12c1a277c8fb0aefd91c";
+
+    error ClaimAirdropScript__InvalidSignatureLength();
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("MerkleAirdrop", block.chainid);
+        claimAirdrop(mostRecentlyDeployed);
+    }
+
+    function claimAirdrop(address airdrop) public {
+        vm.startBroadcast();
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(SIGNATURE);
+        MerkleAirdrop(airdrop).claim(CLAIMING_ADDRESS, AMOUNT_TO_COLLECT, proof, v, r, s);
+        vm.stopBroadcast();
+    }
+
+    function splitSignature(bytes memory sig) public pure returns (uint8 v, bytes32 r, bytes32 s) {
+        if (sig.length != 65) {
+            revert ClaimAirdropScript__InvalidSignatureLength();
+        }
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
+    }
+
+    function splitSignature() public returns (uint8 v, bytes32 r, bytes32 s) {}
+}
